e9058079c2c4d7e27042d24b2ac4512ea74a440d
Narendra <narendrask89@gmail.com>
2019-09-02 20:23:44 +0530

    Added map based service

---
+++ pet-clinic-data/src/main/java/nandu/springframework/petclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,35 @@
+package nandu.springframework.petclinic.services.map;
+
+import java.util.Set;
+
+import nandu.springframework.petclinic.model.Owner;
+import nandu.springframework.petclinic.services.CrudService;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+	@Override
+	public Set<Owner> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Owner findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Owner save(Owner object) {
+		return super.save(object.getId(), object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Owner object) {
+		super.delete(object);
+	}
+
+}
