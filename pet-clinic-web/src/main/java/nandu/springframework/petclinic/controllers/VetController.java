9ae35f1201db4c3e192177754efb126a5e9a19ac
Narendra <narendrask89@gmail.com>
2019-09-07 21:24:24 +0530

    List of vets in index page

--- pet-clinic-web/src/main/java/nandu/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/nandu/springframework/petclinic/controllers/VetController.java
@@ -1,14 +1,27 @@
 package nandu.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import nandu.springframework.petclinic.services.VetService;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
+	private final VetService vetService;
+
+	public VetController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
+
 	@RequestMapping({ "", "/index", "/index.html" })
-	public String listVets() {
+	public String listVets(Model model) {
+		
+		model.addAttribute("vets", vetService.findAll());
+		
 		return "vets/index";
 	}
 }
